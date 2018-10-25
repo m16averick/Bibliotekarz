30d9813e3279010e57fc758b7c25e6c9ba2bb8da
m16averick <298282@uwr.edu.pl>
2018-10-25 22:10:55 +0200

    Nauka formularzy

--- Bibliotekarz/zwrot.Designer.cs
+++ Bibliotekarz/zwrot.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 253);
             this.Name = "zwrot";
-            this.Text = "wypozyczenie";
+            this.Text = "Zwrot";
             this.ResumeLayout(false);
 
         }
